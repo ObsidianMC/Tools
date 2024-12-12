f53fadd1ba40891f23be2c8091d82e243252d8bb
Tides <losttides@gmail.com>
2024-12-11 19:37:14 -0600

    Create ProcessDataComponentsJob.cs

---
+++ SimpleRegistryTransfer/Jobs/ProcessDataComponentsJob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleRegistryTransfer.Jobs;
+
+public sealed class ProcessDataComponentsJob : IProcessJob
+{
+    public async ValueTask Run()
+    {
+        var registry = Helpers.Registries.GetProperty("minecraft:data_component_type");
+
+        var entries = registry.GetProperty("entries");
+
+        var dataComponents = JsonSerializer.Deserialize<Dictionary<string, JsonElement>>(entries.ToString());
+
+        var dataComponentsFile = new FileInfo(Path.Combine(Helpers.OutputPath, "data_components.json"));
+
+        if (dataComponentsFile.Exists)
+            dataComponentsFile.Delete();
+
+        using var dataComponentsWriter = dataComponentsFile.OpenWrite();
+
+        await JsonSerializer.SerializeAsync(dataComponentsWriter, dataComponents);
+    }
+}
